9a0db661a5e48bcf24a0c7ed3fd5a6c0e647a40f
wonchang0314 <developerseok@gmail.com>
2026-05-13 11:05:10 +0900

    chore: 에러 ui 수정 (에러메시지 표시)

--- apps/admin/src/pages/error/ErrorPage.tsx
+++ apps/admin/src/pages/error/ErrorPage.tsx
@@ -1,4 +1,8 @@
-import { useRouteError, isRouteErrorResponse } from "react-router"
+import {
+  useRouteError,
+  isRouteErrorResponse,
+  type ErrorResponse,
+} from "react-router"
 import { FlexBox } from "@/shared/ui/FlexBox"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { RssErrorIcon } from "@hugeicons/core-free-icons"
@@ -12,9 +16,11 @@ export const ErrorPage = () => {
       case 404:
         return <NotFound />
       default:
-        return <NotFound />
+        return <UnexpectedError error={error} />
     }
   }
+
+  return <UnexpectedError error={error as ErrorResponse} />
 }
 
 const NotFound = () => {
@@ -22,7 +28,18 @@ const NotFound = () => {
     <FlexBox direction="column" className="h-screen w-screen justify-center">
       <HugeiconsIcon icon={RssErrorIcon} size={64} className="mb-4" />
       <h1 className="text-4xl font-bold">404</h1>
-      <p className="text-lg">Not Found</p>
+      <p className="text-lg">페이지를 찾을 수 없습니다.</p>
+    </FlexBox>
+  )
+}
+
+const UnexpectedError = ({ error }: { error: ErrorResponse }) => {
+  return (
+    <FlexBox direction="column" className="h-screen w-screen justify-center">
+      <HugeiconsIcon icon={RssErrorIcon} size={64} className="mb-4" />
+      <h1 className="text-4xl font-bold">예상치 못한 오류</h1>
+      <p className="text-lg">예상치 못한 오류가 발생했습니다.</p>
+      <p className="mt-2 text-sm text-gray-500">{error.statusText}</p>
     </FlexBox>
   )
 }
